d2864be86e6bfc1a9f93865ca49cf03c5274788a
redaphid <a@octoblu.com>
2015-07-26 02:02:16 -0700

    omg. alllmost working

--- concat-arduino-libraries.sh
+++ concat-arduino-libraries.sh
@@ -3,13 +3,16 @@ LIB_NAME=tentacle-build
 
 BASE_DIR=`pwd`
 SRC_DIR=$BASE_DIR/test_cpp_files/src
-OUT_DIR=$BASE_DIR/test_cpp_output
+DIST_DIR=$BASE_DIR/dist
 TMP_DIR=$BASE_DIR/tmp
 CONCAT_CMD="gcc -std=c++98 -E -C -P -nostdinc "
 COMBINED_H_FILE=""
 
 rm -rf $TMP_DIR
 mkdir -p $TMP_DIR
+
+rm -rf $DIST_DIR
+mkdir -p $DIST_DIR
 
 for dir in $SRC_DIR/*/
 do
@@ -18,15 +21,6 @@ do
     COMBINED_H_FILE+="#include \"${dir##*/}.h\"\n"
 done
 echo $COMBINED_H_FILE >> $TMP_DIR/$LIB_NAME.hpp
-CONCAT_CMD+=" $TMP_DIR/$LIB_NAME.hpp > $OUT_DIR/$LIB_NAME.hpp 2> $TMP_DIR/errors.txt"
+CONCAT_CMD+=" $TMP_DIR/$LIB_NAME.hpp > $DIST_DIR/$LIB_NAME.h 2> $TMP_DIR/errors.txt"
 
-echo $CONCAT_CMD
-
-
-
-# gcc -std=c++98 -E -C -P -I$SRC_DIR/arduino-nanopb \
-#       -I$SRC_DIR/tentacle \
-#       -I$SRC_DIR/tentacle-arduino \
-#       -I$SRC_DIR/tentacle-pseudopod \
-#       -nostdinc \
-#       $SRC_DIR/tentacle-build.hpp > $OUT_DIR/test.cpp 2> errors.txt
+echo $CONCAT_CMD | sh
